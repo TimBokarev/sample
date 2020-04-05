f9ef5b7708a20acda0d116106ca563f778f25e7e
TimBokarev <tim@bokarev.ru>
2020-04-05 10:03:12 +0200

    Initial commit

---
+++ note.py
@@ -0,0 +1,19 @@
+
+
+file = open("notes.txt", "r")
+current_text = file.read()
+file.close()
+
+added_text = input ('add text: ')
+c = 'print'
+
+if added_text == c :
+   file = open("notes.txt", "r")
+   print(file.read())
+   file.close()
+
+file = open("notes.txt", "w")
+
+file.write(current_text + added_text)
+
+file.close() 
